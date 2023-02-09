4362a02f4ceefdd38840c5614aed4766ff4947b4
swiss0404 <swiss0404@gmail.com>
2023-02-10 01:36:14 +0700

    refactor calculation and open modal by state

--- components/calculation.js
+++ components/calculation.js
@@ -6,19 +6,23 @@ export function adjustedWeight(weight, height) {
 }
 
 export const calculateWeight = (w, h, s) => {
-  if (w > 0) {
-    if (w > 1.2 * getP50weight(h, s)) {
-      console.log("overweight");
-      let TBW = w;
-      let IBW = getP50weight(h, s);
-      let ABW = adjustedWeight(w, h);
-      console.log(TBW, IBW, ABW);
-      return [TBW, IBW, ABW];
+  if (h > 0) {
+    if (w > 0) {
+      if (w > 1.2 * getP50weight(h, s)) {
+        console.log("overweight");
+        let TBW = w;
+        let IBW = getP50weight(h, s);
+        let ABW = adjustedWeight(w, h);
+        console.log(TBW, IBW, ABW);
+        return [TBW, IBW, ABW];
+      } else {
+        console.log("normal weight");
+        return w;
+      }
     } else {
-      console.log("normal weight");
-      return w;
+      return getP50weight(h, s);
     }
   } else {
-    return getP50weight(h, s);
+    return w;
   }
 };
