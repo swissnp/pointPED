183d0ae106aac83504f73239d6af76a18a4b3301
swiss0404 <swiss0404@gmail.com>
2023-02-13 13:10:48 +0700

    workbox

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '/Users/swiss/projects/rama-poison-ped-web',
+	globPatterns: [
+		'**/*.{js,json,ico,svg,webmanifest,md,css,pdf}'
+	],
+	swDest: '/Users/swiss/projects/rama-poison-ped-web/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
