520881711227d6149695582f4e5a6d2adb9e08a6
swiss0404 <swiss0404@gmail.com>
2023-02-16 23:56:28 +0700

    try

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
 	globPatterns: [
 		'**/*.{js,json,ico,svg,webmanifest,md,css,pdf,png}'
 	],
-	swDest: '/Users/swiss/projects/rama-poison-ped-web/sw.js',
+	swDest: '/Users/swiss/projects/rama-poison-ped-web/public/sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
